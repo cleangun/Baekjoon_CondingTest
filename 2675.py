ea4053d45c82631290cecced1d7df91edef40722
GeonGang <50124951+cleangun@users.noreply.github.com>
2023-01-19 07:48:37 +0000

    2675 solving...

--- 2675.py
+++ 2675.py
@@ -9,6 +9,5 @@ for i in range(count):
     for m in range(loop):
       temp_list.append(string_list[j])
       print(f"{string_list[j]}", end="")
-      if(j == len(string_list) -1 and m == loop -1):
+      if (j == len(string_list) - 1 and m == loop - 1):
         print(f"{string_list[j]}")
-    
