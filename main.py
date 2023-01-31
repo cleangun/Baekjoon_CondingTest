211f5c3295058b5e2363fa3c7823ac088564f095
Geon Ahn <50124951+cleangun@users.noreply.github.com>
2023-01-31 07:45:53 +0000

    commit

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-A, B = map(int,input().split())
+import sys
 
-print(A+B)
+input = sys.stdin.readline
+
+count = int(input().strip())
+
+for _ in range(count):
+  answer = 0
+
+  inputData = input().strip().split('X')
+  print(inputData)
+  for i in inputData:
+    if i != '':
+      answer += (len(i) * (len(i) + 1) ) // 2
+
+  print(answer)
