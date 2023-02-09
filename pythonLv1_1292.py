60d5965ca6b842c996cf7d3d5e22b02984acd38d
Geon Ahn <50124951+cleangun@users.noreply.github.com>
2023-02-09 00:42:45 +0000

    1292 solved

--- pythonLv1_1292.py
+++ pythonLv1_1292.py
@@ -1,12 +1,12 @@
-A, B = map(int,input().split())
+A, B = map(int, input().split())
 list = []
 for i in range(1, 1000):
   if i > B:
     break
   for num in range(i):
     list.append(i)
-list.insert(0,0)
+list.insert(0, 0)
 
-for index in range(A,B+1):
+for index in range(A, B + 1):
   list[0] += list[index]
 print(list[0])
