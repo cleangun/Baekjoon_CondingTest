3179dc8cd7395ad8b53c1b12403cb8a32164dc52
Geon Ahn <50124951+cleangun@users.noreply.github.com>
2023-02-12 13:11:36 +0000

    solving 2504 failed

--- main.py
+++ main.py
@@ -1,32 +1,72 @@
-#2581 최적의 속도
 import sys
 
-dmc_list = []
-M = int(sys.stdin.readline())
-N = int(sys.stdin.readline())
-dmc_sum = 0
+stack = []
+str_list = sys.stdin.readline().rstrip()
+answer = 0
 
-for chk_num in range(M,N+1):
-  if chk_num > 1:
-    if chk_num == 2:
-      dmc_list.append(2)
-      continue
-    elif (chk_num % 2 == 0):
-      continue
-    else:
-      error = False
-      for increaseNum in range(3,int(chk_num ** 0.5)+1,2):
-        if not error:
-          if chk_num % increaseNum == 0:
-            error = True
-            continue
+# print(str_list)
+
+# 함수 정의 - 미완성 작업중
+
+for i in str_list:
+  # print("----------------")
+  # print(f" i = {i}")
+  if i == ')':
+    t = 0
+    while len(stack) != 0:
+      top = stack.pop()
+      # print(" if Success!!")
+      # print(f"stack : {stack}")
+      # print(f"top : {top}")
+      if top == "(":
+
+        if t == 0:
+          stack.append(2)
         else:
-          break
-      if error == 0:
-        dmc_list.append(chk_num)
+          stack.append(2 * t)
+        # print(f"t => {t}")
+        # print(f"stack => {stack}")
+        break
+      elif top == "[":
+        print(0)
+        exit(0)
+      else:
+        # print(f"top = {top}")
+        # print(f"int(top) = {int(top)}")
+        t = t + int(top)
 
-if len(dmc_list) == 0:
-  print(-1)
-else:
-  print(sum(dmc_list))
-  print(min(dmc_list))
+  elif i == ']':
+    t = 0
+    while len(stack) != 0:
+      top = stack.pop()
+      # print(" if Success!!")
+      # print(f"stack : {stack}")
+      # print(f"top : {top}")
+      if top == "[":
+        if t == 0:
+          stack.append(3)
+        else:
+          stack.append(3 * t)
+        # print(f"t => {t}")
+        # print(f"stack => {stack}")
+        break
+      elif top == "(":
+        print(0)
+        exit(0)
+      else:
+        # print(f"top = {top}")
+        # print(f"int(top) = {int(top)}")
+        t = t + int(top)
+  else:
+    stack.append(i)
+    # print(f"stack = {stack}")
+
+print(f"fianl : {stack}")
+
+for i in stack:
+  if i == '(' or i == '[':
+    print(0)
+    exit(0)
+  else:
+    answer += i
+print(answer)
