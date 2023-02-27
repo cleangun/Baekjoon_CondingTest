e827250003ed6f9225c4caf244538a95c9a6ff03
Geon Ahn <50124951+cleangun@users.noreply.github.com>
2023-02-27 07:04:31 +0000

    python silver4 1158 solved

--- main.py
+++ main.py
@@ -1,48 +1,19 @@
 import sys
-
+from collections import deque
 input = sys.stdin.readline
 
-N, T = map(int, input().split())
 
-interval = []
+N, K = map(int,input().rstrip().split())
+lis = deque([ i for i in range(1,N+1) ])
 
-for _ in range(N):
-  interval.append(list(map(int, input().rstrip().split())))
+print("<",end="")
+while lis:
+  for _ in range(K-1):
+    lis.append(lis.popleft())
+  if len(lis) == 1:
+    print(lis.popleft(),end="")
+  else:
+    print(lis.popleft(),end = ", ")
 
-late_cnt = 0
-result = 1e9
-
-def remain_AI(st_drive, itv, lst, target):
-  while st_drive < target and lst >= 1:
-    st_drive += itv
-    lst -= 1
-  return st_drive - target
-
-for st_drive, itv, lst in interval:
-  # 도착하자마자
-  if st_drive == T:  # 도착하자마자 버스 있음
-    print(0)
-    exit(0)
+print(">")
   
-  # 운행 후 : 해당 운행이 끝날때보다 늦게 도착 => 탈 수 없음
-  if (st_drive + (itv * (lst-1))) < T:  
-    late_cnt += 1
-    continue
-
-  # 운행 중
-  if st_drive < T:  # 운행시간내에 도착하는데 버스 얼마나 기다려야하는지 계산
-    diff = remain_AI(st_drive, itv, lst - 1, T)
-    if diff < result:
-      result = diff
-  # 운행 전 , 운행 중
-  elif st_drive > T:  # 도착이후에 시작하는 버스들이 얼마나 있는지 확인
-    diff = st_drive - T
-    if diff < result:
-      result = diff
-  
-
-if late_cnt == len(interval):  # 탈 수 있는 버스가 없음
-  print(-1)
-  exit(0)
-
-print(result)
