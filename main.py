654c6d62cbf8f64bbe0dc6ed74d82feaa362353c
Geon Ahn <50124951+cleangun@users.noreply.github.com>
2023-03-18 09:40:20 +0000

    python silver2 1024 solving failed

--- main.py
+++ main.py
@@ -1,40 +1,32 @@
-# 유기농 배추 - 1012 baekjoon
 import sys
-from collections import deque
 
 input = sys.stdin.readline
-T = int(input())
 
+n, l = map(int, input().split())
 
-# function
-def bfs(row, column, maps):
-  queue = deque([])
-  queue.append([row,column])
-  while queue:
-    x, y = queue.popleft()
-    if maps[y][x] == 1:
-      maps[y][x] = 0
-      if x > 0:
-        queue.append([x-1,y])
-      if y > 0:
-        queue.append([x,y-1])
-      if x < (len(maps[0])-1):
-        queue.append([x+1,y])
-      if y < (len(maps)-1):
-        queue.append([x,y+1])
-  return 1
-  
-for _ in range(T):
-  m, n, k = map(int, input().split())
-  maps = [[0] * m for i in range(n)]
-  # 1의 좌표 입력받기:
-  for _ in range(k):
-    x, y = map(int, input().split())
-    maps[y][x] = 1
-    
-  result = 0
-  for y in range(len(maps)):
-    for x in range(len(maps[y])):
-      if maps[y][x] == 1:
-        result += bfs(x, y, maps)
-  print(result)
+start = 0  # 리스트의 시작 인덱스
+end = 0  # 리스트의 끝 인덱스
+sum_list = 0  # 리스트 내의 숫자들의 합
+result = []  # 최종 결과를 저장할 리스트
+
+while start <= end <= n:
+    if len(result) >= l:
+        break
+
+    # 현재 합이 n과 같거나 더 큰 경우 start를 1 증가시키고 sum_list에서 start번째 값을 빼준다.
+    if sum_list >= n:
+        sum_list -= start
+        start += 1
+    # 현재 합이 n보다 작은 경우 end를 1 증가시키고 sum_list에 end번째 값을 더해준다.
+    elif sum_list < n:
+        end += 1
+        sum_list += end
+
+    # 만약 리스트의 길이가 l 이상이고, 합이 n과 같은 경우 결과 리스트에 start부터 end까지의 값을 저장하고 더 이상 계산하지 않는다.
+    if len(result) >= l and sum_list == n:
+        result = list(range(start, end+1))
+
+if not result:
+    print(-1)
+else:
+    print(' '.join(map(str, result)))
