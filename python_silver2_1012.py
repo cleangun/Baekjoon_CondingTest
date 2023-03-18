4f838339beb37f1b066a1795fb61ac9361567a9a
Geon Ahn <50124951+cleangun@users.noreply.github.com>
2023-03-18 04:55:48 +0000

    python silver2 standard 1012 solved

--- python_silver2_1012.py
+++ python_silver2_1012.py
@@ -9,21 +9,22 @@ T = int(input())
 # function
 def bfs(row, column, maps):
   queue = deque([])
-  queue.append([row,column])
+  queue.append([row, column])
   while queue:
     x, y = queue.popleft()
     if maps[y][x] == 1:
       maps[y][x] = 0
       if x > 0:
-        queue.append([x-1,y])
+        queue.append([x - 1, y])
       if y > 0:
-        queue.append([x,y-1])
-      if x < (len(maps[0])-1):
-        queue.append([x+1,y])
-      if y < (len(maps)-1):
-        queue.append([x,y+1])
+        queue.append([x, y - 1])
+      if x < (len(maps[0]) - 1):
+        queue.append([x + 1, y])
+      if y < (len(maps) - 1):
+        queue.append([x, y + 1])
   return 1
-  
+
+
 for _ in range(T):
   m, n, k = map(int, input().split())
   maps = [[0] * m for i in range(n)]
@@ -31,7 +32,7 @@ for _ in range(T):
   for _ in range(k):
     x, y = map(int, input().split())
     maps[y][x] = 1
-    
+
   result = 0
   for y in range(len(maps)):
     for x in range(len(maps[y])):
