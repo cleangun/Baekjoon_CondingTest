67528b066ecd6cb866c5d11aefa7aff0404cbe31
Geon Ahn <50124951+cleangun@users.noreply.github.com>
2023-03-19 08:59:10 +0000

    python silver2 1024 sequence sum solved

--- main.py
+++ main.py
@@ -4,29 +4,21 @@ input = sys.stdin.readline
 
 n, l = map(int, input().split())
 
-start = 0  # 리스트의 시작 인덱스
-end = 0  # 리스트의 끝 인덱스
-sum_list = 0  # 리스트 내의 숫자들의 합
-result = []  # 최종 결과를 저장할 리스트
+# https://hooongs.tistory.com/334
+# => N = (x+1) + (x+2) + (x+3) + ... + (x+L)
+# => N = Lx + L(L+1)/2
+# => Lx = N - (L * (L+1) / 2)
 
-while start <= end <= n:
-    if len(result) >= l:
-        break
+for i in range(l, 101):
+  x = n - (i * (i + 1) / 2)
 
-    # 현재 합이 n과 같거나 더 큰 경우 start를 1 증가시키고 sum_list에서 start번째 값을 빼준다.
-    if sum_list >= n:
-        sum_list -= start
-        start += 1
-    # 현재 합이 n보다 작은 경우 end를 1 증가시키고 sum_list에 end번째 값을 더해준다.
-    elif sum_list < n:
-        end += 1
-        sum_list += end
+  if x % i == 0:
+    x = int(x / i)
 
-    # 만약 리스트의 길이가 l 이상이고, 합이 n과 같은 경우 결과 리스트에 start부터 end까지의 값을 저장하고 더 이상 계산하지 않는다.
-    if len(result) >= l and sum_list == n:
-        result = list(range(start, end+1))
-
-if not result:
-    print(-1)
+    if x >= -1:  # x+1, x+2 이렇게 가니까 0부터 나와야하니까 -1보다 크거                   # 나 같아야함
+      for j in range(1, i + 1):
+        print(x + j, end=" ")
+      print()
+      break
 else:
-    print(' '.join(map(str, result)))
+  print(-1)
--- python_silver2_1024.py
+++ python_silver2_1024.py
@@ -1,47 +1,24 @@
-# 수열의 합 - silver2 baekjoon
-# => 시간을 빨리하기 위해, (투 포인터 알고리즘 사용)
 import sys
 
 input = sys.stdin.readline
 
-
-#function
-def get_sequence(start_num, target_num):
-  li = [start_num]
-  while sum(li) < target_num:
-    most_num = li[-1]
-    li.append(most_num + 1)
-  return li
-
-
-def binary_search(node, left, right, final_li):
-  if (right < left) or (left > right):
-    return final_li
-  mid = (right + left) // 2
-
-  func_result = get_sequence(mid, n)
-  if (sum(func_result) == n) and (len(func_result) >= l):
-    if len(final_li) == 0:
-      final_li = func_result
-    elif len(func_result) < len(final_li):
-      final_li = func_result
-  # 오른쪽 탐색
-  temp = binary_search(node, mid + 1, right, final_li)
-  if final_li != temp:
-    final_li = temp
-  # 왼쪽 탐색
-  else:
-    final_li = binary_search(node, left, mid - 1, final_li)
-
-  return final_li
-
-
 n, l = map(int, input().split())
 
-node = list([i for i in range(0, n + 1)])
-result = binary_search(node, 0, n // l, [])
-if len(result) == 0:
-  print(-1)
+# https://hooongs.tistory.com/334
+# => N = (x+1) + (x+2) + (x+3) + ... + (x+L)
+# => N = Lx + L(L+1)/2
+# => Lx = N - (L * (L+1) / 2)
+
+for i in range(l, 101):
+  x = n - (i * (i + 1) / 2)
+
+  if x % i == 0:
+    x = int(x / i)
+
+    if x >= -1:  # x+1, x+2 이렇게 가니까 0부터 나와야하니까 -1보다 크거                   # 나 같아야함
+      for j in range(1, i + 1):
+        print(x + j, end=" ")
+      print()
+      break
 else:
-  for i in result:
-    print(i, end=" ")
+  print(-1)
