c4c9850fe6f90b284d194bb16a448fd7a263c56f
Geon Ahn <50124951+cleangun@users.noreply.github.com>
2023-03-23 07:01:45 +0000

    coding study bignumRule best

--- python_offline_bignumRule.py
+++ python_offline_bignumRule.py
@@ -7,6 +7,5 @@ n, m, k = map(int, input().split())
 arr = list(map(int, input().split()))
 
 arr.sort()
-max_multier = m - (m // (k + 1))
-min_multier = m // (k + 1)
-print((max_multier * arr[-1]) + (min_multier * arr[-2]))
+print( ((m -m // (k + 1)) * arr[-1] ) + 
+			( (m // (k + 1)) * arr[-2]) )
