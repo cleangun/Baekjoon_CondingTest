fc00eeebd7184467e3b2afbc5c35fe1dd3f55a76
Geon Ahn <50124951+cleangun@users.noreply.github.com>
2023-03-24 11:58:40 +0000

    python gold5 1107 almost solving

--- main.py
+++ main.py
@@ -1,22 +1,64 @@
-# # baekjoon 1980 - 햄버거사랑
+# baekjoon gold5 1107 - 리모컨
+import sys
+input = sys.stdin.readline
 
-result = []
-n, m, t = map(int,input().split())
+#function
+def find_more(full_num,last_num,numlist):
+  # last_num = "457" / numlist [ 1 2 3 5 9 0]
+  small = 0
+  big = 0
+  for turn in range(2):
+    for i in range(len(last_num)-1,-1,-1):
+      mid = int(last_num[0])
+      if turn == 0:  # 작은 수를 구할 차례
+        if i == (len(last_num)-1):
+          while mid >= 0:
+            if (mid < int(last_num[0])) and (mid in numlist):
+              small += mid * (10**i)
+              break
+            mid -= 1
+        else:
+          if i == 0:
+            small += max(numlist)
+          else:
+            small += max(numlist) * (10**i)
+      else:  # 큰수를 구할 차례
+        if i == (len(last_num)-1):
+          while mid <= 9:
+            if (mid > int(last_num[0])) and (mid in numlist):
+              big += mid * (10**i)
+              break
+            mid += 1
+        else:
+          if i == 0:
+            big += min(numlist)
+          else:
+            big += min(numlist) * (10**i)
+  return min([big,small], key=lambda x: abs(x - t))
 
-if t < n and t < m:
-  print(f"0 {t}")
-  exit(0)   # 추가된 부분
+#main
+t = int(input())
+n = int(input())
+first_cnt = abs(int(t)-100)  # + - 버튼을 이용했을 때 횟수
 
-for coffee in range(0,t):
-  pas = False
-  target = t - coffee
-  for tow in range(target//n,-1,-1):
-    bul = (target - (tow*n)) // m
-    if ((bul*m)+(tow*n)) == target:
-      result.append(((bul+tow),coffee))
-      pas = True
-  if pas == True:
+numlist = list([i for i in range(0,10)])
+if n != 0:
+  for no_num in list(map(int,input().split())):
+    numlist.remove(no_num)
+
+# 예외 처리
+if t == 100:
+  print(0)
+  exit(0)
+
+combine = ""
+for idx in range(len(str(t))):
+  if int(str(t)[idx]) in numlist:
+    combine += str(t)[idx]
+  else:  # 해당 수가 없는 경우
+    last_num = str(t)[idx:]
+    combine += str(find_more(t,last_num,numlist))
     break
-result.sort()
-cnt,cof = result.pop(-1)
-print(cnt,end=" "); print(cof)
+print(abs(t-int(combine)) + len(combine))
+    
+      
