2f51302cb011a1acdffa6a2430d8f5251694f5f5
Geon Ahn <50124951+cleangun@users.noreply.github.com>
2023-07-02 13:59:57 +0900

    [Silver III] Title: 터렛, Time: 40 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Silver/1002. 터렛/터렛.py
+++ 백준/Silver/1002. 터렛/터렛.py
@@ -9,33 +9,13 @@ T = int(input())
 for _ in range(T):
     x1,y1,r1,x2,y2,r2 = map(int,input().split())
     distance = sqrt((x1-x2)**2 + (y1-y2)**2)
-    isSameNode = False
-    if (x1 == x2) and (y1 == y2):
-        isSameNode = True
-    if (r1 == 0) or (r2 == 0):
-        if r1 == r2 == 0:
-            if distance == 0:
-                print(1)
-            else:
-                print(0)
-        else:
-            if distance == max(r1,r2):
-                print(1)
-            else:
-                print(0)
-    # 두 노드의 좌표가 같을 때
-    elif isSameNode:
-        if r1 != r2:
-            print(0)
-        elif r1 == r2:
-            print(-1)
-    # 두 노드의 좌표가 다를 때
+    if (distance == 0) and (r1 == r2):
+        print(-1)        
+    elif (r1 + r2 == distance) or (abs(r1 - r2) == distance):
+        print(1)
+    elif abs(r1 - r2) < distance < (r1 + r2):
+        print(2)
     else:
-        if (r1 + r2 == distance) or (abs(r1 - r2) == distance):
-            print(1)
-        elif abs(r1 - r2) < distance < (r1 + r2):
-            print(2)
-        else:
-            print(0)
+        print(0)
     
     
