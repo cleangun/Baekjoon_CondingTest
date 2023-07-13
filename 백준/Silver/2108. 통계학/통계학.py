13b2162e98e98d0e34a3a69c3d5c16d300b5ca95
Geon Ahn <50124951+cleangun@users.noreply.github.com>
2023-07-13 15:15:37 +0900

    [Silver III] Title: 통계학, Time: 480 ms, Memory: 52640 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -1,4 +1,5 @@
 import sys
+from collections import Counter
 input = sys.stdin.readline
 
 numlst = []
@@ -7,21 +8,35 @@ n = int(input())
 for _ in range(n):
     numlst.append(int(input()))
 numlst.sort()
-cntDict = {}
-for num in numlst:
-    if num not in cntDict:
-        cntDict[num] = 1
-    else:
-        cntDict[num] += 1
-max_keys = [key for key,value in cntDict.items() if value == max(cntDict.values())]
+
+# Way 1
+# cntDict = {}
+# for num in numlst:
+#     if num not in cntDict:
+#         cntDict[num] = 1
+#     else:
+#         cntDict[num] += 1
+# max_keys = [key for key,value in cntDict.items() if value == max(cntDict.values())]
+
 # 산술평균
 print(round(sum(numlst) / n))
 # 중앙값
 print(numlst[n//2])
 # 최빈값
-if len(max_keys) > 1:
-    print(max_keys[1])
+
+# Way1
+# if len(max_keys) > 1:
+#     print(max_keys[1])
+# else:
+#     print(max_keys[0])
+
+# Way2
+cnt = Counter(numlst)
+most_Nums = cnt.most_common()
+if len(most_Nums) > 1 and most_Nums[0][1] == most_Nums[1][1]:
+    print(most_Nums[1][0])
 else:
-    print(max_keys[0])
+    print(most_Nums[0][0])
+    
 # 범위
 print(abs(max(numlst) - min(numlst)))
