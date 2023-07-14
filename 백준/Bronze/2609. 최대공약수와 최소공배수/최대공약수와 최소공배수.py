21912a4e10bb0830a00fbab36e4f969de0b0f082
Geon Ahn <50124951+cleangun@users.noreply.github.com>
2023-07-14 12:42:22 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -6,18 +6,11 @@ n1,n2 = map(int,input().split())
 minnum = min(n1,n2)
 maxnum = max(n1,n2)
 
-d = 1
-while (minnum // d) > 0:
+num = 1
+for d in range(1,minnum+1):
     num = minnum // d
     if (minnum % num) == 0:
         if (maxnum % num) == 0:
             print(num)
             break
-    d += 1
-d = 1
-while True:
-    num = maxnum * d
-    if (num % minnum) == 0:
-        print(num)
-        break
-    d += 1
+print(n1*n2//num)
