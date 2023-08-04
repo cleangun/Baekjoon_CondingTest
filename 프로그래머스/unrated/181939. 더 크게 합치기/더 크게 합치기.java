bbf4baa2b275c5f9eeccb9831e7575f466b76172
Geon Ahn <50124951+cleangun@users.noreply.github.com>
2023-08-04 12:53:21 +0900

    [unrated] Title: 더 크게 합치기, Time: 8.76 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int a, int b) {
-        int 후보1 = Integer.valueOf(String.valueOf(a) + String.valueOf(b));
-        int 후보2 = Integer.valueOf(String.valueOf(b) + String.valueOf(a));
-        return Math.max(후보1,후보2);
+        int along = Integer.parseInt(""+a+b);
+        int blong = Integer.parseInt(""+b+a);
+        return along > blong? along : blong;
     }
 }
